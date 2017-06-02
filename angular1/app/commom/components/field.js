b8413375fd5a7bbff0855c0685283238d556d671
Administrador <administrador@MacBook-de-Administrador.local>
2017-06-02 20:45:18 -0300

    error calndario nao abre

--- angular1/app/commom/components/field.js
+++ angular1/app/commom/components/field.js
@@ -7,21 +7,22 @@
       placeholder: '@',
       type: '@',
       model: '=',
+      readonly: '<',
     },
     controller: [
       'gridSystem',
       function(gridSystem) {
-        this.gridSystem = gridSystem.toCssClasses(this.grid)
+        this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
       }
     ],
     template: `
-      <div class= "{{ $ctrl.gridClasses }}">
-        <div class="form-group">
-          <label for="{{ $ctrl.id }}">{{$ctrl.label}}</label>
-          <input id="{{$ctrle.id}}" class="form-control" placeholder="{{$ctrl.placeholder}}"
-            type="{{$ctrl.type}}" ng-model="$ctrl.model"/>
-        </div>
+    <div class="{{ $ctrl.gridClasses }}">
+      <div class="form-group">
+        <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+        <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
+          type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly">
       </div>
+    </div>
     `
   })
 })()
